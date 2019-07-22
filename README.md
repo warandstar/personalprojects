4a1f6f20290ce4fa5ccbf1f8521c974c853e634b
JongTaiKim <jtk7@uw.edu>
2019-07-21 18:00:41 -0800

    add readme.md and random_box.R and finish for now

---
+++ README.md
@@ -0,0 +1,5 @@
+random_box is about simulating the following problem:
+There are n (number) boxes and there are blue and red balls. Each box has different number of each blue and red balls with fixed number of n; so "first" box has 0 red balls, "second" box has 1 red ball, ..., and "nth" box has n red balls.
+We randomly pick the box out of n number of boxes and then we randomly pick the ball with that chosen box.
+What is probability of choosing red ball by doing such step?
+My guess is 50% as there certainly about n/2 red balls, so the fact that we are choosing random box is a misguiding fact.
---
+++ random_box.R
@@ -0,0 +1,26 @@
+# brief explanation of this simulation can be found in README.md in this folder
+# red ball is 1, blue ball is 0 for this
+
+# amount_box : number of boxes in this simulation
+# times: how many times we run the simulation
+amount_box <- 7
+times <- 10000
+
+# initiate the boxes and number of red balls in each box
+boxes <- 1:amount_box
+
+results <- NULL
+box_numbers <- NULL 
+# simulation
+for (i in 1:times) {
+  box_number <- sample(1:amount_box, 1)
+  balls_box <- c(rep(1, box_number), rep(0, amount_box - box_number))
+  picked_ball <- balls_box[sample(1:amount_box, 1)]
+
+  # store numbers
+  box_numbers <- c(box_numbers, box_number)
+  results <- c(results, picked_ball)
+}
+
+hist(box_numbers)
+prob <- sum(results) / times
